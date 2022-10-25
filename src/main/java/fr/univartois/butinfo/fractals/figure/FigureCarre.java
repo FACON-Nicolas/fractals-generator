cc6a3801149514abbe17424405aa127f4a7e310d
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2022-10-25 14:28:48 +0200

    ajout carré

---
+++ src/main/java/fr/univartois/butinfo/fractals/figure/FigureCarre.java
@@ -0,0 +1,35 @@
+/**
+ * Ce fichier fait partie du projet sae-2022-2023.
+ *
+ * (c) 2022 nicolas
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.fractals.figure;
+
+import java.awt.Color;
+
+/**
+ * Le type FigureCarre
+ *
+ * @author nicolas
+ *
+ * @version 0.1.0
+ */
+public class FigureCarre extends FigureRectangle {
+
+    /**
+     * Crée une nouvelle instance de FigureCarre.
+     * @param x
+     * @param y
+     * @param color
+     * @param width
+     * @param height
+     */
+    public FigureCarre(int x, int y, Color color, int width) {
+        // TODO Auto-generated constructor stub.
+        super(x, y, color, width, width);
+    }
+
+}
+
