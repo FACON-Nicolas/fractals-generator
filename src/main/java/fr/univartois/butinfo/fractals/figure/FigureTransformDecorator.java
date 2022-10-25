af456587423b1ae70f5d29bc75a40cf435b0fa39
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2022-10-25 14:29:47 +0200

    ajout decorateur transform

---
+++ src/main/java/fr/univartois/butinfo/fractals/figure/FigureTransformDecorator.java
@@ -0,0 +1,21 @@
+/**
+ * Ce fichier fait partie du projet sae-2022-2023.
+ *
+ * (c) 2022 nicolas
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.fractals.figure;
+
+
+/**
+ * Le type FigureTransformDecorator
+ *
+ * @author nicolas
+ *
+ * @version 0.1.0
+ */
+public class FigureTransformDecorator {
+
+}
+
