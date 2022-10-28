a9a216716428a93a354fb791cbd0eba28368cc5d
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2022-10-28 11:44:34 +0200

    modif couleur

--- src/main/java/fr/univartois/butinfo/fractals/figure/AbstractFigure.java
+++ src/main/java/fr/univartois/butinfo/fractals/figure/AbstractFigure.java
@@ -15,4 +15,34 @@ public abstract class AbstractFigure implements IFigure {
         this.y = y;
         this.color = color;
     }
+    
+    
+    /**
+     * Donne l'attribut x de cette instance de AbstractFigure.
+     *
+     * @return L'attribut x de cette instance de AbstractFigure.
+     */
+    public int getX() {
+        return x;
+    }
+    
+    
+    /**
+     * Donne l'attribut y de cette instance de AbstractFigure.
+     *
+     * @return L'attribut y de cette instance de AbstractFigure.
+     */
+    public int getY() {
+        return y;
+    }
+    
+    
+    /**
+     * Donne l'attribut color de cette instance de AbstractFigure.
+     *
+     * @return L'attribut color de cette instance de AbstractFigure.
+     */
+    public Color getColor() {
+        return color;
+    }
 }
