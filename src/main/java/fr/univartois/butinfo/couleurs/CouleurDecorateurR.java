2e3ba09036cc495a8f0d089990b997f19a718cf5
Elsa Logier <elsa.logier@gmail.com>
2022-10-28 09:31:36 +0200

    commentaires de la classe CouleurDecorateurR

--- src/main/java/fr/univartois/butinfo/couleurs/CouleurDecorateurR.java
+++ src/main/java/fr/univartois/butinfo/couleurs/CouleurDecorateurR.java
@@ -3,10 +3,19 @@ package fr.univartois.butinfo.couleurs;
 import java.awt.Color;
 
 public class CouleurDecorateurR extends CouleurDecorateur{
-
+	/**
+	 * Crée une instance de la classe 
+	 * @param palette une palette de couleurs
+	 */
 	public CouleurDecorateurR(IStrategieCouleurs palette) {
 		super(palette);
 	}
+	
+	/**
+	 * Crée une palette de couleur et met un masque sur la couleur verte
+	 * @param nbItMax le nombre d'itérations max
+	 * @param nbIterations le nombre d'itérations
+	 */
 	@Override
 	public Color palette(int nbItMax, int nbIterations) {
 		Color couleur = palette.palette(nbItMax, nbIterations);
