6a3ccb708397024fad923cfea911129855101623
Elsa Logier <elsa.logier@gmail.com>
2022-10-28 09:45:20 +0200

    ajout des commentaires de classe pour IStrategieCouleurs et CouleurDecorateur

--- src/main/java/fr/univartois/butinfo/couleurs/CouleurDecorateur.java
+++ src/main/java/fr/univartois/butinfo/couleurs/CouleurDecorateur.java
@@ -1,7 +1,12 @@
 package fr.univartois.butinfo.couleurs;
 
 import java.awt.Color;
-
+/**
+ * 
+ * @author Elsa
+ *La classe CouleurDécorateur permet de générer un masque sur une palette de couleur (rouge vert ou bleu)
+ *C'est un décorateur.
+ */
 public abstract class CouleurDecorateur implements IStrategieCouleurs{
 	 protected IStrategieCouleurs palette;
 	 
--- src/main/java/fr/univartois/butinfo/couleurs/IStrategieCouleurs.java
+++ src/main/java/fr/univartois/butinfo/couleurs/IStrategieCouleurs.java
@@ -1,6 +1,11 @@
 package fr.univartois.butinfo.couleurs;
 
 import java.awt.Color;
+/**
+ * 
+ * @author Elsa
+ *On utilise un patron de stratégie.
+ */
 public interface IStrategieCouleurs {
 
 	public Color palette(int nbItMax, int nbIterations);
