9b0b6cbfb519b6471944c4664d1852e8b8b9e3aa
Arnaud Fievet <arnaud_fievet@ens.univ-artois.fr>
2022-10-28 09:59:12 +0200

    correction scripts de lancement

--- mandelbrot.sh
+++ mandelbrot.sh
@@ -8,9 +8,7 @@ nbIterations=100
 directoryOutput="./$nomFractale"
 nbZoom=5
 scale=$(echo "2/$height" | bc -l)
-palette=rose
-focusX=-1.4002
-focusY=0
+
 
 # Permet de créer le jar permettant de générer les fractales
 ./gradlew jar
@@ -21,37 +19,14 @@ then
   mkdir "$directoryOutput"
 fi
 
-while [[ $# -gt 0 ]]; do
-  case $1 in
-    --help)
-      java -jar build/libs/sae-2022-2023.jar --help
-      exit 1
-      ;;
-    -p|--palette-name)
-      palette="$2"
-      shift
-      shift
-      ;;
-    -x|--focus-x)
-      focusX="$2"
-      shift
-      shift
-      ;;
-    -y|--focus-y)
-      focusY="$2"
-      shift
-      shift
-      ;;
-  esac
-done
 
 for ((i=1;i<=nbZoom;i++))
 do
   fileName="$nomFractale-$i.png"
 
-  java -jar build/libs/sae-2022-2023.jar -f $nomFractale -h "$height" -w "$width" -n "$nbIterations" \
-  -o "$directoryOutput/$fileName" -p "$palette" -x "$focusX" -y "$focusY" -s "$scale"
+  java -jar build/libs/sae-2022-2023.jar -f $nomFractale -h $height -w $width -n $nbIterations \
+  -o $directoryOutput/$fileName -s "$scale" "$@"
 
-  scale=$(echo "$scale/2" | bc -l)
+  scale=$(echo "$scale/1.5" | bc -l)
 
 done
