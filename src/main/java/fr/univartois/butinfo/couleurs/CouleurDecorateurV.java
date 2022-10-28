4e51cd37d676136a51b264973a0731673b357b43
Elsa Logier <elsa.logier@gmail.com>
2022-10-28 09:25:10 +0200

    commentaire de la méthode du masque vert

--- src/main/java/fr/univartois/butinfo/couleurs/CouleurDecorateurV.java
+++ src/main/java/fr/univartois/butinfo/couleurs/CouleurDecorateurV.java
@@ -7,6 +7,12 @@ public class CouleurDecorateurV extends CouleurDecorateur {
 	public CouleurDecorateurV(IStrategieCouleurs palette) {
 		super(palette);
 	}
+	
+	/**
+	 * Crée une palette de couleur et met un masque sur la couleur verte
+	 * @param nbItMax le nombre d'itérations max
+	 * @param nbIterations le nombre d'itérations
+	 */
 	@Override
 	public Color palette(int nbItMax, int nbIterations) {
 		Color couleur = palette.palette(nbItMax, nbIterations);
